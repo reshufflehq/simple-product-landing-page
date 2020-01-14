2ceb9b68e3191fe75073bcdbe406725e0030da7d
zolga <olga.kolopanov@gmail.com>
2020-01-14 10:52:28 -0800

    CR: Use import, comment db-admin, fix time to ISO

--- backend/_handler.js
+++ backend/_handler.js
@@ -1,20 +1,22 @@
 import express from 'express';
+import exphbs from 'express-handlebars';
+import moment from 'moment';
 import Validator from 'fastest-validator';
-const db = require('@reshuffle/db');
-import path from 'path';
+import { get, find, Q, update, create, remove } from '@reshuffle/db';
 import { defaultHandler } from '@reshuffle/server-function';
-import { schema } from './schema';
-import { defaultStyle } from './constants';
 import { authHandler } from '@reshuffle/passport';
-const moment = require('moment');
-const devDBAdmin = require('@reshuffle/db-admin');
-var exphbs = require('express-handlebars');
+import devDBAdmin from '@reshuffle/db-admin';
+import { defaultStyle } from './constants';
+import { schema } from './schema';
 
 const validator = new Validator();
 const app = express();
 
 app.set('trust proxy', true);
-app.use('/db/db-admin', express.json(), devDBAdmin.devDBAdminHandler);
+
+//For developing/testing only
+// app.use('/db/db-admin', express.json(), devDBAdmin.devDBAdminHandler);
+
 app.use(authHandler);
 
 app.engine('handlebars', exphbs());
@@ -22,15 +24,15 @@ app.set('view engine', 'handlebars');
 app.use(express.static('public'));
 
 app.get('/', async function(req, res) {
-  let texts = await db.get('texts');
+  let texts = await get('texts');
   res.render('index', { texts: texts });
 });
 
 app.get('/admin', async function(req, res) {
   if (req.user) {
-    let texts = await db.get('texts');
-    const allKeysQuery = db.Q.filter(db.Q.key.startsWith('user/'));
-    const result = await db.find(allKeysQuery);
+    let texts = await get('texts');
+    const allKeysQuery = Q.filter(Q.key.startsWith('user/'));
+    const result = await find(allKeysQuery);
     res.render('admin', {
       user: req.user,
       data: result,
@@ -59,17 +61,16 @@ app.post('/register', express.json(), async function(req, res) {
     res.status(200).send(valid[0].message);
   } else {
     let key = `user/${req.body.email}`;
-    let time = moment().format('MMMM Do YYYY, h:mm:ss a');
+    let time = moment().toISOString(true);
     let value = { email: req.body.email, time: time };
-    await db.create(key, value);
+    await create(key, value);
     res.sendStatus(200);
   }
 });
 
-//todo protect this endpoint
 app.post('/update', express.json(), async function(req, res) {
   let key = `texts`;
-  const created = await db.update(key, () => {
+  const created = await update(key, () => {
     const body = { ...req.body };
     body['primary'] = body['primary'] || defaultStyle.primary;
     body['secondary'] = body['secondary'] || defaultStyle.secondary;
@@ -82,7 +83,7 @@ app.post('/update', express.json(), async function(req, res) {
 
 app.post('/delete', express.json(), async function(req, res) {
   let key = `user/${req.body.email}`;
-  await db.remove(key);
+  await remove(key);
   res.sendStatus(200);
 });
 
